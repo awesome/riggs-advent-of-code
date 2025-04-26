1232fb41812a2fdf948d4495a06dd6825fd9ad87
SoAwesomeMan <soawesomeman@me.com>
2025-04-26 16:50:31 -0700

    [test fail]Add parse_input method and corresponding test
    Introduced the parse_input method in Day1 to handle input parsing. Added a test case to verify its behavior, replacing the placeholder test.

--- aoc2015-day1/lib/aoc2015/day1.rb
+++ aoc2015-day1/lib/aoc2015/day1.rb
@@ -2,4 +2,9 @@ module Aoc2015; end
 
 class Aoc2015::Day1
   VERSION = "1.0.0"
+
+  def self.parse_input(input)
+    return false
+  end
+
 end
--- aoc2015-day1/test/aoc2015/test_day1.rb
+++ aoc2015-day1/test/aoc2015/test_day1.rb
@@ -5,7 +5,9 @@ require "aoc2015/day1"
 module TestAoc2015; end
 
 class TestAoc2015::TestDay1 < Minitest::Test
-  def test_sanity
-    flunk "write tests or I will kneecap you"
+  def test_parse_input
+    exp = {"(" => 2, ")" => 2}
+    act = Aoc2015::Day1.parse_input('(())')
+    assert_equal exp, act
   end
 end
