8adfb2e4ea212c9dab191a6afd3f920da5db1e63
SoAwesomeMan <soawesomeman@me.com>
2025-04-27 10:40:34 -0700

    [test false]Add initial implementation and tests for Day2 module
    Introduce `box`, `extra_paper`, and `solve` methods in the `Day2` module, returning placeholder values for now. Added corresponding tests to validate these methods against basic expected outputs.

--- aoc2015-day2/lib/aoc2015/day2.rb
+++ aoc2015-day2/lib/aoc2015/day2.rb
@@ -1,5 +1,19 @@
 module Aoc2015; end
 
-class Aoc2015::Day2
+module Aoc2015::Day2
   VERSION = "1.0.0"
+
+  module_function
+
+  def box(input)
+    return false
+  end
+
+  def extra_paper(input)
+    return false
+  end
+
+  def solve(input)
+    return false
+  end
 end
--- aoc2015-day2/test/aoc2015/test_day2.rb
+++ aoc2015-day2/test/aoc2015/test_day2.rb
@@ -5,7 +5,40 @@ require "aoc2015/day2"
 module TestAoc2015; end
 
 class TestAoc2015::TestDay2 < Minitest::Test
-  def test_sanity
-    flunk "write tests or I will kneecap you"
+  def test_box
+    input = "2x3x4"
+    exp = 52
+    act = Aoc2015::Day2.box(input)
+    assert_equal exp, act
+
+    input = "1x1x10"
+    exp = 42
+    act = Aoc2015::Day2.box(input)
+    assert_equal exp, act
   end
+
+  def test_extra_paper
+    input = "2x3x4"
+    exp = 6
+    act = Aoc2015::Day2.extra_paper(input)
+    assert_equal exp, act
+
+    input = "1x1x10"
+    exp = 1
+    act = Aoc2015::Day2.extra_paper(input)
+    assert_equal exp, act
+  end
+
+  def test_solve
+    input = "2x3x4"
+    exp = 58
+    act = Aoc2015::Day2.solve(input)
+    assert_equal exp, act
+
+    input = "1x1x10"
+    exp = 43
+    act = Aoc2015::Day2.solve(input)
+    assert_equal exp, act
+  end
+
 end
